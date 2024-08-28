b5bd3c07e9ab9b144f3c5805af7bd86329f295fd
onsdagens <pawdzi-7@student.ltu.se>
2024-08-28 15:29:35 +0200

    add attributes

--- macros/examples/test.rs
+++ macros/examples/test.rs
@@ -1,12 +1,54 @@
-use syncrim_mips_rt_macros::text;
+use syncrim_mips_rt_macros::*;
 
 fn main() -> ! {
     loop {}
 }
-
+mod SOME {}
+mod SOME {}
 #[text]
 fn some_fn() -> u32 {
-    let a = 32;
-    let b = a + 50;
+    // some
+    let a = 1;
+    let b = a + 2;
     b
 }
+
+#[text]
+fn some_other_fn() -> u32 {
+    // some other
+    let a = 3;
+    let b = a + 4;
+    b
+}
+
+#[ktext]
+fn some_kernel_fn() -> u32 {
+    // some kernel
+    let a = 5;
+    let b = a + 6;
+    b
+}
+
+#[ktext]
+fn some_other_kernel_fn() -> u32 {
+    // some other kernel
+    let a = 7;
+    let b = a + 8;
+    b
+}
+
+#[data]
+static STATIC: u8 = 1;
+#[data]
+static mut STATIC_MUT: u32 = 1;
+#[data]
+pub static PUB_STATIC: usize = 1 + 2;
+#[data]
+pub static mut PUB_STATIC_MUT: bool = true;
+
+
+
+#[data]
+const CONST: u32 = 1;
+#[data]
+pub const PUB_CONST: bool = true;
--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -2,9 +2,11 @@
 #[macro_use]
 extern crate quote;
 use proc_macro::TokenStream;
-use syn::ItemFn;
+use syn::{ItemFn, ItemConst, ItemStatic};
 #[macro_use]
 extern crate syn;
+
+
 #[proc_macro_attribute]
 pub fn text(_: TokenStream, input: TokenStream) -> TokenStream {
     let f = parse_macro_input!(input as ItemFn);
@@ -21,4 +23,51 @@ pub fn text(_: TokenStream, input: TokenStream) -> TokenStream {
         }
     )
     .into()
+}
+
+#[proc_macro_attribute]
+pub fn ktext(_: TokenStream, input: TokenStream) -> TokenStream {
+    let f = parse_macro_input!(input as ItemFn);
+
+    let name = f.sig.ident;
+    let stmts = f.block.stmts;
+    let unsafety = f.sig.unsafety;
+    let ret = f.sig.output;
+    quote!(
+        #[link_section = ".ktext"]
+        #[no_mangle]
+        #unsafety fn #name () #ret {
+            #(#stmts)*
+        }
+    )
+    .into()
+}
+
+#[proc_macro_attribute]
+pub fn data(_: TokenStream, input: TokenStream) -> TokenStream {
+    if input.to_string().contains("const") {
+        let i = parse_macro_input!(input as ItemConst);
+        let ty = i.ty;
+        let vis = i.vis;
+        let expr = i.expr;
+        let ident = i.ident;
+        quote!(
+            #[link_section = ".data"]
+            #[no_mangle]
+            #vis const #ident: #ty = #expr;
+        ).into()
+    } else {
+        let i = parse_macro_input!(input as ItemStatic);
+        let ty = i.ty;
+        let vis = i.vis;
+        let expr = i.expr;
+        let ident = i.ident;
+        let mutability = i.mutability;
+        quote!(
+            #[link_section = ".data"]
+            #[no_mangle]
+            #vis static #mutability #ident: #ty = #expr;
+        ).into()
+    }
+    //let i = parse_macro_input!(input as ItemConst);
 }
